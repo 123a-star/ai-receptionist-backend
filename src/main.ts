096fa3f26fe7a66e1359d5045913d5ff8d8a06c1
saurabh tripathi <saurabhtripathi1706@gmail.com>
2026-05-08 17:57:37 +0530

    Applied serverless pattern for Vercel

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
+const expressApp = express();
+let isInit = false;
+
+// Ye function NestJS ko poori tarah load karne ka kaam karega
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Frontend ke liye CORS
-  app.enableCors();
-
-  // Vercel apna port khud assign karega, warna local par 3000 chalega
-  await app.listen(process.env.PORT || 3000);
+  if (!isInit) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    app.enableCors();
+    await app.init();
+    isInit = true;
+  }
 }
-bootstrap();
+
+
+if (!process.env.VERCEL) {
+  bootstrap().then(() => {
+    expressApp.listen(3000, () => {
+      console.log('Local server running on port 3000');
+    });
+  });
+}
+
+
+export default async function (req: any, res: any) {
+  await bootstrap();
+  expressApp(req, res);
+}
